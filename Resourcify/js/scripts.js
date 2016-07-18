8ae9c46df03a21a0899fda04c81e5cf8dc5d02b1
mihbor <mbor81@gmail.com>
2016-07-18 23:18:50 +0100

    #4

--- Resourcify/js/scripts.js
+++ Resourcify/js/scripts.js
@@ -9,4 +9,15 @@ function clicked() {
    clickBalance+=1;
    localStorage["clickBalance"] = clickBalance;
    clickBalanceElement.innerHTML = clickBalance;
+   updateMissionStatus();
+}
+function updateMissionStatus() {
+	if(honourBalance==0){
+	 var mission1progressElement = document.getElementById("mission1progress");
+	 mission1progressElement.value = clickBalance;
+	 if(clickBalance>=100){
+		 honourBalance+=1;
+		 mission1progressElement.style="display:none";
+	 }
+   }
 }
