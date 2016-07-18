3a0e613e74be5db05671346acf684b71c5586bb2
mihbor <mbor81@gmail.com>
2016-07-18 19:07:05 +0100

    #4

--- Resourcify/js/scripts.js
+++ Resourcify/js/scripts.js
@@ -1,6 +1,9 @@
 var clickBalance = 0;
 if(localStorage["clickBalance"]) clickBalance = parseInt(localStorage["clickBalance"]);
 
+var honourBalance = 0;
+if(localStorage["honourBalance"]) honourBalance = parseInt(localStorage["honourBalance"]);
+
 function clicked() {
    var clickBalanceElement = document.getElementById("click-balance");
    clickBalance+=1;
