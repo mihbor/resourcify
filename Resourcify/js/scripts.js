32cba14b6c0397e17efa434f65764b9859fba430
mihbor <mbor81@gmail.com>
2016-07-24 10:40:41 +0100

    #5

--- Resourcify/js/scripts.js
+++ Resourcify/js/scripts.js
@@ -5,22 +5,32 @@ var honourBalance = 0;
 if(localStorage["honourBalance"]) honourBalance = parseInt(localStorage["honourBalance"]);
 
 function clicked() {
-   var clickBalanceElement = document.getElementById("click-balance");
    clickBalance+=1;
    localStorage["clickBalance"] = clickBalance;
-   clickBalanceElement.innerHTML = clickBalance;
+   updateBalance();
    updateMissionStatus();
+}
+
+function updateBalance(){
+	var clickBalanceElement = document.getElementById("click-balance");
+	clickBalanceElement.innerHTML = clickBalance;
 }
 function updateMissionStatus() {
 	if(honourBalance==0){
-	 var mission1progressElement = document.getElementById("mission1progress");
-	 mission1progressElement.value = clickBalance;
-	 if(clickBalance>=100){
+	  var mission1progressElement = document.getElementById("mission1progress");
+	  mission1progressElement.value = clickBalance;
+	  if(clickBalance>=100){
 		 honourBalance+=1;
-		 var honourBalanceElement = document.getElementById("honour-balance");
-		 honourBalanceElement.innerHTML=honourBalance;
 		 mission1progressElement.style="display:none";
 		 document.getElementById("honour-balanceRow").style="";
-	 }
+	  }
+	  var honourBalanceElement = document.getElementById("honour-balance");
+	  honourBalanceElement.innerHTML=honourBalance;
    }
+}
+function resetProgress() {
+	clickBalance = 0;
+	honourBalance = 0;
+	updateBalance();
+	updateMissionStatus();
 }
